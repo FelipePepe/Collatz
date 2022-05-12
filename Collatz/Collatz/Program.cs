8745b6565ddf5f043823204bd4ee73fef33cca87
Felipe Pérez <felipepepe@gmail.com>
2022-05-12 21:14:30 +0200

    Inicio

---
+++ Collatz/Collatz/Program.cs
@@ -0,0 +1,31 @@
+﻿Func<long, bool> isEven = (n) => n % 2 == 0;
+
+Action<long> collatz = null;
+
+collatz = (n) =>
+{
+    Console.WriteLine(n);
+
+    long result = 0;
+
+    if (isEven(n))
+    {
+        result = n / 2;
+    }
+    else
+    {
+        result = (n * 3) + 1;
+    }
+
+    if (result != 1)
+    {
+        collatz(result);
+    }
+    else
+    {
+        Console.WriteLine(result);
+        return;
+    }
+};
+
+collatz(130);
