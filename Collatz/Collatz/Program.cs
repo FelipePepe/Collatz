99b80ebff22bf6279e31642d1546e12659fa517c
Felipe Pérez <felipepepe@gmail.com>
2022-05-12 21:22:23 +0200

    Refactorización

--- Collatz/Collatz/Program.cs
+++ Collatz/Collatz/Program.cs
@@ -5,26 +5,9 @@ Action<long> collatz = null;
 collatz = (n) =>
 {
     Console.WriteLine(n);
-
-    long result = 0;
-
-    if (isEven(n))
+    if (n != 1)
     {
-        result = n / 2;
-    }
-    else
-    {
-        result = (n * 3) + 1;
-    }
-
-    if (result != 1)
-    {
-        collatz(result);
-    }
-    else
-    {
-        Console.WriteLine(result);
-        return;
+        collatz(isEven(n) ? (n / 2) : (n * 3) + 1);
     }
 };
 
